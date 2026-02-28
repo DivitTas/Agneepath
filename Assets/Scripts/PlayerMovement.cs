ad5611d9a2f5a251c8f27e4553e3a330f9c6771e
DivitTas <divit.nt@gmail.com>
2026-03-01 01:41:39 +0530

    Finish basic dash and movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,21 +8,55 @@ public class PlayerMovement : MonoBehaviour
 {
     public InputSystem_Actions action;
     public Rigidbody2D rb;
+    int facingDirection = 1; // 1 = right, -1 = left
+
     public float speedMultiplier = 9f;
     public float acceleration = 40f;
     public float deceleration = 200;
-    public float jumpForce = 20f;
 
+    public float jumpForce = 20f;
     public Transform groundCheck;
     public float groundCheckRadius = 0.1f;
     public LayerMask groundLayer;
-    private void OnEnable()
+
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.2f;
+    public float dashCooldown = 1f;
+    private bool isDashing;
+    private float dashTimeLeft;
+    private float dashCooldownLeft;
+    private float originalGravity;
+
+    private void Awake()
     {
         action = new InputSystem_Actions();
-        action.Enable();
-        action.Player.Jump.performed += OnJump;
         rb = GetComponent<Rigidbody2D>();
     }
+    private void OnEnable()
+    {
+        action.Enable();
+        action.Player.Jump.performed += OnJump;
+        action.Player.Dash.performed += OnDash;
+    }
+
+    private void OnDash(InputAction.CallbackContext context)
+    {
+        TryDash();
+    }
+    private void StartDash()
+    {
+        isDashing = true;
+        dashTimeLeft = dashDuration;
+        dashCooldownLeft = dashCooldown;
+        originalGravity = rb.gravityScale;
+        rb.gravityScale = 0;
+    }
+    private void EndDash()
+    {
+        isDashing = false;
+        rb.gravityScale = originalGravity;
+    }
+
 
     private void OnJump(InputAction.CallbackContext context)
     {
@@ -33,6 +67,7 @@ public class PlayerMovement : MonoBehaviour
     {
         action.Disable();
         action.Player.Jump.performed -= OnJump;
+        action.Player.Dash.performed -= OnDash;
     }
 
 
@@ -45,7 +80,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //nothign yet ;p
+        if (dashCooldownLeft > 0f)
+        {
+        dashCooldownLeft -= Time.deltaTime;
+        }
     }
 
     Vector2 HandleInput()
@@ -55,12 +94,27 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        
         Vector2 input = HandleInput();
+        if (input.x > 0.01f)
+            facingDirection = 1;
+        else if (input.x < -0.01f)
+            facingDirection = -1;
+        if (isDashing)
+        {
+            rb.linearVelocity = new Vector2(facingDirection * dashSpeed, 0);
+            dashTimeLeft -= Time.fixedDeltaTime;
+            if(dashTimeLeft <= 0)
+            {
+                EndDash();
+            }
+            return;
+        }
         float targetSpeed = input.x * speedMultiplier;
         float currentSpeed = rb.linearVelocity.x;
         float accelRate;
         // later implement logic to round speed to 9 if greater than 7.5 for controller stick drift adjustment
-        if (Mathf.Abs(targetSpeed) > 0.01f)
+        if (Mathf.Abs(targetSpeed) < 0.01f)
         {
             accelRate = deceleration;
         }
@@ -80,5 +134,13 @@ public class PlayerMovement : MonoBehaviour
     bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void TryDash()
+    {
+        if(isDashing) return;
+        if (dashCooldownLeft > 0) return;
+        StartDash();
+        
     }
 }
